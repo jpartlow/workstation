81b02f345f45301f69fecf30439eddf0744e8399
Josh Partlow <joshua.partlow@puppetlabs.com>
2016-01-29 17:14:52 -0800

    Remove r10k class
    Do not need to involve r10k.  The puppet module tool will expand
    dependencies from the metadata in an install.

--- manifests/r10k.pp
+++
@@ -1,7 +0,0 @@
-# Install r10k gem so that we can manage module dependencies, and not write everything
-# from scratch.
-class workstation::r10k {
-  package { 'r10k':
-    provider => 'puppet_gem'
-  }
-}
