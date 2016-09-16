348f2b22d447f4cfb23fecfd8fc772114ad1e9cb
Josh Partlow <joshua.partlow@puppetlabs.com>
2016-09-16 14:01:58 -0700

    Get base spec passing
    Added module requirements to .fixtures.yaml for rspec-puppet.  Added
    hieradata for future specs, not currently used.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
-describe 'workstation' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('workstation') }
+describe 'workstation' do
+  let(:params) do
+    {
+      "account"            => "rspec",
+      "repository_sources" => ["git@github.com:some/repo"],
+    }
   end
+
+  it { is_expected.to compile.with_all_deps }
+  it { is_expected.to contain_class('Rbenv') }
+  it { is_expected.to contain_rbenv__build('2.1.8') }
+  it { is_expected.to contain_package('git') }
+  it { is_expected.to contain_user('rspec') }
+  it { is_expected.to contain_vcsrepo('/home/rspec/work/src/repo') }
 end
