ad85a9d447e9326f96c922c74424347fad94fb7a
Josh Partlow <joshua.partlow@puppetlabs.com>
2016-09-16 13:53:48 -0700

    Add git and repos
    Control user account from the top.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,38 +6,26 @@
 # Parameters
 # ----------
 #
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'workstation':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
+# @param account [String] Account name to ensure is created.
+# @param repository_sources [Array<String>] Of repository source urls.
 #
 # Authors
 # -------
 #
 # Josh Partlow <joshua.partlow@puppetlabs.com>
 #
-class workstation {
+class workstation(
+  String $account,
+  Array[String] $repository_sources,
+){
   include workstation::ruby
-  include workstation::user
+  class { workstation::user:
+    account => $::workstation::account,
+  }
+  contain workstation::user
+  contain workstation::git
+  workstation::repos { "main":
+    repository_sources => $::workstation::repository_sources,
+    require            => Class["Workstation::Git"],
+  }
 }
