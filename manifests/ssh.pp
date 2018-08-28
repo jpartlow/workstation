c3f4bd6a0d42fdb616d1461ede67e9d90fa37ecf
Josh Partlow <joshua.partlow@puppetlabs.com>
2018-08-27 17:32:22 -0700

    (maint) Remove unused insecure ssh key handling
    There was some broken puppet code meant to manage installing insecure
    private key files for ssh. Since it was unused, I've removed it.

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -1,8 +1,7 @@
 class workstation::ssh(
   Array[Array[String]] $public_keys,
-  Array[String] $insecure_private_keys = [],
   String $user  = $::workstation::user::account,
-  String $sshdir = "/home/${user}/.ssh"
+  String $sshdir = "/home/${user}/.ssh",
 ) {
   
   file { $sshdir:
@@ -14,18 +13,10 @@ class workstation::ssh(
     $comment = $pub[0]
     $key = $pub[1]
     ssh_authorized_key { $comment:
-      ensure  => present,
-      user    => $user,
-      type    => 'ssh-rsa',
-      key     => $key,
-    }
-  }
-
-  $insecure_private_keys.each |$key| {
-    file { "${sshdir}/${file}":
-      ensure => 'present',
-      content => $key,
-      mode   => '0600',
+      ensure => present,
+      user   => $user,
+      type   => 'ssh-rsa',
+      key    => $key,
     }
   }
 
