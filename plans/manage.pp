6fa6614577c0df20b3f50aad48ea3677c3712803
Josh Partlow <joshua.partlow@puppetlabs.com>
2019-03-06 16:40:37 -0800

    (maint) Return results of applying the manage catalog
    And log what Puppet did. If this is a successful run, the Puppet log
    output is provided. If it's a failed run, we get Bolt's error summary
    instead (since we're not catching errors), which will only display the
    resource information we errored on.

--- plans/manage.pp
+++ plans/manage.pp
@@ -6,11 +6,34 @@ plan workstation::manage(
   apply_prep($nodes)
 
   get_targets($nodes).each |$target| {
-    apply($target) {
+    $results = apply($target, _catch_errors => false) {
       $target_account = lookup(workstation::account)
       notice("Generating a workstation on ${target} for ${target_account}")
       warning("This may take several minutes, depending on how many Ruby installations are being downloaded and compiled for this workstation, and how many repositories are being cloned.")
       include workstation
     }
+    $results.each() |$result| {
+      notice("Logs from: ${result.target}")
+      $report = $result.report()
+      $logs = $report['logs']
+      $logs.each() |$log| {
+        $message = "${log['source']}: ${log['message']}"
+        case $log['level'] {
+          'notice': {
+            notice($message)
+          }
+          'warning': {
+            warning($message)
+          }
+          'err': {
+            error($message)
+          }
+          default: {
+            # skip info/debug
+          }
+        }
+      }
+    }
+    return $results
   }
 }
