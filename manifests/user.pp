6cfb919f27452452924a21bca1c968b06b505c1e
Josh Partlow <joshua.partlow@puppetlabs.com>
2019-07-18 16:52:18 -0700

    (maint) Make the user class work on el
    Pick groups used for sudo access based on os family.

--- manifests/user.pp
+++ manifests/user.pp
@@ -10,14 +10,24 @@
 # @param groups [Array<String>] Of groups the account should belong to.  Default is 'sudo'.
 class workstation::user(
   String $account,
-  Array[String] $groups = ['sudo','admin'],
   String $shell = '/bin/bash',
+  Optional[Array[String]] $groups = undef,
 ) {
+
+  $default_groups = case $facts['os']['family'] {
+    'RedHat': { [ 'adm', 'wheel'] }
+    default: { ['sudo', 'admin'] }
+  }
+
+  $_groups = $groups =~ Undef ? {
+    true    => $default_groups,
+    default => $groups,
+  }
 
   user { $account:
     ensure     => present,
     managehome => true,
-    groups     => $groups,
+    groups     => $_groups,
     shell      => $shell,
   }
 }
