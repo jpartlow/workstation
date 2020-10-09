689e7b5e14c01fdb1130fcb0137ed5b66e488c0b
Josh Partlow <joshua.partlow@puppetlabs.com>
2020-10-09 12:50:20 -0700

    (maint) Update manage plan to apply a list of profiles
    The workstation::profiles key in hiera is namespaced so that a lookup
    can find it ('profiles' won't be found, for example). This is a just an
    array of classes to include. So it's a simple hiera based node
    classification method.

--- plans/manage.pp
+++ plans/manage.pp
@@ -2,26 +2,29 @@
 # is managed as a workstation, based on the hieradata configuration.
 #
 # @param nodes [TargetSpec] nodes to manage.
-# @param yes [Boolean] if true, skip user confirmation (for automated
-# workflows).
 plan workstation::manage(
   TargetSpec $nodes,
-  Boolean $yes = false,
 ) {
   apply_prep($nodes)
 
   get_targets($nodes).each |$target| {
     out::message("Generating a workstation on ${target}")
-    out::message("Will configure ${target} based on hiera data found for data/nodes/${target.facts['clientcert']}.yaml")
+    out::message(" * Will configure ${target} based on hiera data found for data/nodes/${target.facts['clientcert']}.yaml")
     $message = @(WARNING/L)
-      This may take several minutes, depending on how many Ruby installations \
+       * This may take several minutes, depending on how many Ruby installations \
       are being downloaded and compiled for this workstation, and how many \
       repositories are being cloned.
       |-WARNING
     out::message($message)
 
     $results = apply($target, _catch_errors => true) {
-      include workstation
+      $profiles = lookup('workstation::profiles', 'default_value' => [])
+      if empty($profiles) {
+        fail("No 'profiles' found in data/nodes/${target.facts['clientcert']}.yaml. Nothing to apply.")
+      }
+      $profiles.each |$p| {
+        contain $p
+      }
     }
 
     workstation::display_apply_results($results.first())
